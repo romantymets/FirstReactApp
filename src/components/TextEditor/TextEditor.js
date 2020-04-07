6e39cb286a882607232727e888dea81a97a71a5f
romantymets <romantymets@gmail.com>
2020-04-07 19:26:12 +0300

    init react app

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -8,12 +8,41 @@ import './TextEditor.css';
 class TextEditor extends React.Component {
   constructor() {
     super();
+    this.state = {
+      colortextArea : '',
+      colorText: 'black',
+      text: "",
+      isMouseOver:false,
+    }
+    this.onTextChange = this.onTextChange.bind(this);
+    this.onMouseOverTextArea = this.onMouseOverTextArea.bind(this);
   }
+  onTextChange (e) {
+    const inputtext = e.target.value;
+    this.setState({text:inputtext});
+  }
+  onMouseOverTextArea () {
+    this.setState({isMouseOver: true})
+    console.log('-----> on onMouseOverButton', );
+  }
+  onMouseLeaveTextArea = () => {
+    this.setState({isMouseOver: false})
+  };
   render () {
-
+const colorArea = this.state.colortextArea;
+const text = this.state.text;
+    const isMouseOver = this.state.isMouseOver;
     return (
       <div className="text-editor-container" >
-       I a text editor
+
+        <textarea
+          style={{backgroundColor : colorArea}}
+          onChange={this.onTextChange}
+          onMouseLeave={this.onMouseLeaveTextArea}
+          onMouseOver={this.onMouseOverTextArea}
+          className={isMouseOver ? "text-editor-container1" : "text-editor-container"}
+        >  </textarea>
+        <p> {text} </p>
       </div>
     );
   }
