e50ff211aa7886284911604f75c9c1116779fc38
romantymets <romantymets@gmail.com>
2020-04-22 10:33:06 +0300

    add new style

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
-import TodoList from "./Todo/Todolist";
+// import TodoList from "./Todo/Todolist";
 class App extends React.Component {
   constructor() {
     super();
     // стан комонента
     this.state = {
-      textHere: "",
-      todos: []
+      newInputText: "",
+      todos: [],
+
     };
   }
   onTextChange = (e) => {
-     this.setState({ textHere: e.target.value })
+     this.setState({ newInputText: e.target.value,})
+
   };
   onButtonClick =() => {
-    const newInputText = this.state.textHere;
+    const newInputText = this.state.newInputText;
     const oldTodos = this.state.todos;
-    this.setState({todos:[...oldTodos,newInputText]})
+     this.setState({
+       todos:[...oldTodos,newInputText]});
+
 
   }
   render () {
-    const newInputText = this.state.textHere;
+    const newInputText = this.state.newInputText;
     const todos = this.state.todos;
     return (
       <div className="app" >
         <h1> helo world </h1>
-        <div className="but">
+        <div className="button-conteiner">
           <input type={"text"} onChange={this.onTextChange}>
 
           </input>
           <button className="btn btn-primary" type={"button"} onClick={this.onButtonClick}>
             Add todo
           </button>
-          <ul>
-            {todos.map((todo) =>
-              (<li>
-                  {todo}
+          <ul className="ul-container">
+            {todos.map((todo, index) =>
+              (<li className="li-conteiner">
+                <span>
+                     <input type="checkbox" className="checkbox-container"/> {index + 1}.
+                       <strong className="strong-container">
+                         {todo}
+                       </strong>
+                </span>
+
+                <button className="btn btn-primary"> Delete </button>
               </li>)
             )}
           </ul>
         </div>
+
       </div>
     );
   }
--- src/Todo/Todolist.js
+++ src/Todo/Todolist.js
@@ -1,21 +1,20 @@
-import React from "react";
-import PropTypes from 'prop-types'
-import TodoItem from "../Todo/TodoItem";
-const styles = {
-  url: {
-    listStyle: 'none',
-    margin: 0,
-    padding: 0
-  }
-}
+// import React from "react";
+// import PropTypes from 'prop-types'
+// import TodoItem from "../Todo/TodoItem";
+// const styles = {
+//   url: {
+//     listStyle: 'none',
+//     margin: 0,
+//     padding: 0
+//   }
+// }
 
-export default function Todolist (props) {
-  return(
-    <ul style={styles.url}>
-      { props.todos.map((todo, index) => {
-       return <TodoItem todo={todo} key={todo.id} index={index + 1}/>
-        })}
-    </ul>
-  )
-
-}
+// export default function Todolist (props) {
+//   return(
+//     <ul style={styles.url}>
+//       { props.todos.map((todo, index) => {
+//        return <TodoItem todo={todo} key={todo.id} index={index + 1}/>
+//         })}
+//     </ul>
+//   )
+// }
