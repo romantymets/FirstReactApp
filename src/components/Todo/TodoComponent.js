a6778fc0cb859fdbd813e05f57446cbeb0ebebd7
romantymets <romantymets@gmail.com>
2020-05-31 18:49:41 +0300

    refactor code

--- src/components/Todo/TodoComponent.js
+++ src/components/Todo/TodoComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DatePicker from "react-datepicker/es";
 import {SketchPicker} from 'react-color'
+import _ from 'lodash'
 import TodoList from "../TodoList/TodoList";
 import './Todo.css';
 
@@ -65,22 +66,29 @@ class Todo extends React.Component {
     });
     this.setState({ todos: newArray })
   };
-  onItemRemove = (id) => (e) => {
-    const newArray = [];
-    this.state.todos.forEach(todo => {
-      if (todo.id !== id) {
-        newArray.push(todo)
-      }
-    });
-    const newIndexArray = newArray.map((todo, index) => {
-      return {
-        ...todo,
-        id: index + 1
-      }
-    });
-    this.setState({ todos: newIndexArray })
-  };
+  // onItemRemove = (id) => (e) => {
+  //   const newArray = [];
+  //   this.state.todos.forEach(todo => {
+  //     if (todo.id !== id) {
+  //       newArray.push(todo)
+  //     }
+  //   });
+  //   const newIndexArray = newArray.map((todo, index) => {
+  //     return {
+  //       ...todo,
+  //       id: index + 1
+  //     }
+  //   });
+  //   this.setState({ todos: newIndexArray })
+  // };
 
+  onItemRemove = (id) => (e) => {
+    const newindex = _.findIndex(this.state.todos,(todo) => todo.id === id);
+    const newTodos = [...this.state.todos];
+    newTodos.splice(newindex, 1);
+    this.setState({ todos: newTodos.map((todo,index) => ({...todo, id:index + 1 }))})
+  };
+  
   render() {
     const todos = this.state.todos;
     return (
